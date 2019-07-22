5031b91a870de1b428fc22663cbd66f3096d6252
huangmy <540759982@qq.com>
2019-07-22 23:51:13 +0800

    数据库

--- application/api/model/Banner.php
+++ application/api/model/Banner.php
@@ -14,8 +14,22 @@ use think\Exception;
 
 class Banner
 {
- public  static function  getBannerByID($id){
-     $result=  Db::query('select*from banner_item where banner_id=?',[$id]);
-     return $result;
- }
+    public static function getBannerByID($id)
+    {
+//     $result=  Db::query('select*from banner_item where banner_id=?',[$id]);
+//     return $result;
+//        $result = Db::table('banner_item')
+//            ->where('banner_id', '=', $id)
+//            ->select();
+//      where('字段名','表达式','查询条件');
+        //表达式,数组发,闭包
+        //     updete,delete,insert,find
+        $result = Db::table('banner_item')
+//            ->fetchSql()
+            ->where(function ($query) use ($id) {
+                $query->where('banner_id', '=', $id);
+            })
+            ->select();
+        return $result;
+    }
 }
